40ade34230bb131fa7cdaaae478bf8837109f492
dhjw <deanhjw@gmail.com>
2024-01-02 23:53:35 -0800

    Skip link titles for some captcha pages

--- bot.php
+++ bot.php
@@ -1778,7 +1778,7 @@ while (1) {
 					$title = preg_replace('/(&#[0-9]+;)/', '', $title);
 					$title = str_replace(["\r\n", "\n", "\t", "\xC2\xA0"], ' ', $title);
 					$title = trim(preg_replace('/\s+/', ' ', $title));
-					$notitletitles = ['imgur: the simple image sharer', 'Imgur', 'Imgur: The most awesome images on the Internet', 'Login • Instagram', 'Access denied .* used Cloudflare to restrict access', 'Amazon.* Something Went Wrong.*', 'Sorry! Something went wrong!', 'Bloomberg - Are you a robot?', 'Attention Required! | Cloudflare', 'Access denied', 'Access Denied', 'Please Wait... | Cloudflare', 'Log into Facebook', 'DDOS-GUARD', 'Just a moment...', 'Amazon.com', 'Amazon.ca', 'Blocked - 4plebs', 'MSN', 'Access to this page has been denied', 'You are being redirected...', 'Instagram'];
+					$notitletitles = [$parse_url["host"], 'imgur: the simple image sharer', 'Imgur', 'Imgur: The most awesome images on the Internet', 'Login • Instagram', 'Access denied .* used Cloudflare to restrict access', 'Amazon.* Something Went Wrong.*', 'Sorry! Something went wrong!', 'Bloomberg - Are you a robot?', 'Attention Required! | Cloudflare', 'Access denied', 'Access Denied', 'Please Wait... | Cloudflare', 'Log into Facebook', 'DDOS-GUARD', 'Just a moment...', 'Amazon.com', 'Amazon.ca', 'Blocked - 4plebs', 'MSN', 'Access to this page has been denied', 'You are being redirected...', 'Instagram'];
 					foreach ($notitletitles as $ntt) {
 						if (preg_match('/^' . str_replace('\.\*', '.*', preg_quote($ntt)) . '$/', $title)) {
 							echo "Skipping output of title: $title\n";
