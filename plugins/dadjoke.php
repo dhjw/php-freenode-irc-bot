4ec049f8757049775b861fe467752cbad9edf3a2
dhjw <deanhjw@gmail.com>
2024-05-03 23:05:22 -0700

    Fix plugin

--- plugins/dadjoke.php
+++ plugins/dadjoke.php
@@ -7,7 +7,7 @@ $custom_triggers[] = ['!dad', 'function:dadjoke', true, '!dad - output a random 
 function dadjoke()
 {
 	global $target, $curl_info, $api_ninjas_key;
-	$r = json_decode(curlget([CURLOPT_URL => 'https://api.api-ninjas.com/v1/dadjokes?limit=1', CURLOPT_HTTPHEADER => ["X-Api-Key: $api_ninjas_key"]]));
+	$r = json_decode(curlget([CURLOPT_URL => 'https://api.api-ninjas.com/v1/dadjokes', CURLOPT_HTTPHEADER => ["X-Api-Key: $api_ninjas_key"]]));
 	if ($curl_info['RESPONSE_CODE'] == 200 && isset($r[0]->joke)) {
 		$joke = str_replace(["\r\n", "\n", "\t"], ' ', $r[0]->joke);
 		$joke = str_replace(['“', '”'], '"', $joke);
